fad4285b612a15e9244d13795dfbd6af90d68a1b
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-01-31 14:01:47 -0500

    completed the saveEdit and deleteColor functions. Also, created a form at bottom of colorList to add new colors

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,17 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/Login";
 import "./styles.scss";
 
-import PrivateRoute from "./components/PrivateRoute";
-import BubblePage from "./components/BubblePage"
+import PrivateFoute from "./components/PrivateRoute";
+import BubblePage from "./components/BubblePage";
+import ColorList from "./components/ColorList";
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
-        <PrivateRoute path="/bubble" component={BubblePage} />
+        <PrivateFoute path="/bubble" component={BubblePage} />
+        {/* <PrivateFoute path="/bubble/:id" component={ColorList} /> */}
       </div>
     </Router>
   );
--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -6,13 +6,16 @@ import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
-  axiosWithAuth().get('http://localhost:5000/api/colors')
-    .then(res => {
-      console.log(res.data)
-      setColorList(res.data)
-    })
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("http://localhost:5000/api/colors")
+      .then(res => {
+        console.log(res.data);
+        setColorList(res.data);
+      });
+  }, [setColorList]);
+
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
