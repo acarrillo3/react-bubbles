37c5ecfa689570cf83c5130a540335e1267a6372
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-01-31 12:26:03 -0500

    created login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -7,6 +7,11 @@ const Login = () => {
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <form>
+        <label htmlFor="username">Username: <input type="text" name="username" /> </label>
+        <label htmlFor="password">Password: <input type="text" name="password" /> </label>
+        <button>Login</button>
+      </form>
     </>
   );
 };
